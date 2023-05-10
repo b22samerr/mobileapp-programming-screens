626593c1ed7125823a928ca0ecffc2599a8c77cc
b22samer <b22samer@student.his.se>
2023-05-10 08:45:06 +0200

    Gjorde så att man kan besöka MainActivity.java från Activity2.java

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, Activity2.class);
-                intent.putExtra("name", "Ergul"); // Option
+                intent.putExtra("name", "Ergul"); // Optionn
                 startActivity(intent);
             }
         });
